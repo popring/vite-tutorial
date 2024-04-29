8af207f3b4ec2fa6e2618c7a12904d936785731d
popring <koler778@gmail.com>
2024-04-29 17:36:24 +0800

    chapter5, lint

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 // index.tsx
 // import styles from './index.module.scss';
 import { devDependencies } from '../../../package.json';
+// import { useState } from 'react';
 
 export function Header() {
   return (
     <div>
       {/* <p className={styles.header}>This is Header</p> */}
 
-      <div className='p-20 text-center'>
-        <h1 className='font-bold text-2xl mb-2'>
+      <div className="p-20 text-center">
+        <h1 className="font-bold text-2xl mb-2">
           vite version: {devDependencies.vite}
         </h1>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig, normalizePath } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
 import path from 'path';
-// import autoprefixer from 'autoprefixer';
+// @ts-ignore
+import viteEslint from 'vite-plugin-eslint';
+// import viteStylelint from 'vite-plugin-stylelint';
 
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
@@ -12,27 +14,24 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 export default defineConfig({
   root: path.resolve(__dirname),
   plugins: [
-    react(),
+    viteEslint(),
+    react()
+    // viteStylelint({
+    //   // 对某些文件排除检查
+    //   exclude: '/windicss|node_modules/'
+    // }),
   ],
   css: {
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`,
-      },
+        additionalData: `@import "${variablePath}";`
+      }
     },
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
       // 其中，name 表示当前文件名，local 表示类名
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-    },
-    // postcss: {
-    //   plugins: [
-    //     autoprefixer({
-    //       // 指定目标浏览器
-    //       overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
-    //     }),
-    //   ],
-    // },
-  },
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    }
+  }
 });
